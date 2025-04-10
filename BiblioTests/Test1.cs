4514e7e879161a807e4cc0f74d063dc6052644cc
mathis <mathis>
2025-04-10 07:55:58 -0400

    test unitaires

---
+++ BiblioTests/Test1.cs
@@ -0,0 +1,39 @@
+﻿using JeuxList;
+
+namespace BiblioTests
+{
+    [TestClass]
+    public sealed class Connection
+    {
+        [TestMethod]
+        public void adminConnexion()
+        {
+            DatabaseConnection dbConnection = new DatabaseConnection();
+
+            // Information de connexion
+            User username = new User("admin");
+            var password = "hash_admin_password";
+
+            UserCheck userCheck = new UserCheck().identification(username, dbConnection, User.HashPassword(password));
+
+            Assert.AreEqual(1, userCheck.status);
+        }
+
+        [TestMethod]
+
+        public void userConnexion()
+        {
+            DatabaseConnection dbConnection = new DatabaseConnection();
+
+            // Information de connexion
+            User username = new User("alice");
+            var password = "1234";
+
+            UserCheck userCheck = new UserCheck().identification(username, dbConnection, User.HashPassword(password));
+
+            Assert.AreEqual(2, userCheck.status);
+        }
+    }
+
+
+}
--- JeuxList/obj/Debug/net8.0-windows/JeuxList.AssemblyInfo.cs
+++ JeuxList/obj/Debug/net8.0-windows/JeuxList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JeuxList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eae18017f072db01d07d8ae1b431f00ab8cc247")]
 [assembly: System.Reflection.AssemblyProductAttribute("JeuxList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JeuxList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
